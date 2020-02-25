92bf0b33952b9b3033674545499c4205d75d7ffb
Kartikey <parasharkartikey@gmail.com>
2020-02-25 18:47:07 +0530

    zero feet should return zero inch

--- Quantity_Measurement_Deck.java
+++ Quantity_Measurement_Deck.java
@@ -1,4 +1,13 @@
 // Welcome to Quantity Measurement Program
 public class Quantity_Measurement_Deck {
+    private int feet;
 
+    public Quantity_Measurement_Deck(int feet) {
+        this.feet = feet;
+    }
+
+    public int toInch() {
+
+        return feet*12;
+    }
 }
