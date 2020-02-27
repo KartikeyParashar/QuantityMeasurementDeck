90cc3664ea00afab111ea2bee71867eff76784bd
Kartikey <parasharkartikey@gmail.com>
2020-02-27 16:56:49 +0530

    Added testcases in UC1 branch

--- Quantity_Measurement_Deck.java
+++ Quantity_Measurement_Deck.java
@@ -1,13 +1,38 @@
 // Welcome to Quantity Measurement Program
-public class Quantity_Measurement_Deck {
-    private int feet;
+public class Quantity_Measurement_Deck
+{
+    private Object feet;
+    private Object inch;
 
-    public Quantity_Measurement_Deck(int feet) {
-        this.feet = feet;
+    public Object function_for_returning_value_of_feet(Object feet)
+    {
+        if (feet instanceof Integer)
+        {
+            int value = (int) feet;
+            return value;
+        }
+        return "Null Value";
     }
 
-    public int toInch() {
+    public Object function_for_returning_value_of_inch(Object inch)
+    {
+        if (inch instanceof Integer)
+        {
+            int value = (int) inch;
+            return value;
+        }
+        return "Null Value";
+    }
 
-        return feet*12;
+    public Object function_for_converting_feet_toInch(Object feet)
+    {
+        int feet_to_inch = (int) feet * 12;
+        return feet_to_inch;
+    }
+
+    public Object function_for_converting_inch_toFeet(Object inch)
+    {
+        int inch_to_feet = (int) inch / 12;
+        return inch_to_feet;
     }
 }
--- Quantity_Measurement_DeckTest.java
+++ Quantity_Measurement_DeckTest.java
@@ -2,9 +2,115 @@ import static org.junit.Assert.*;
 
 public class Quantity_Measurement_DeckTest {
 
+    Quantity_Measurement_Deck object_of_QM = new Quantity_Measurement_Deck();
+
+    //.....................................TEST CASES FOR FEET.........................................................
     @org.junit.Test
-    public void toInch_check_for_zero() throws Exception {
-        Quantity_Measurement_Deck feet = new Quantity_Measurement_Deck(0);
-        assertEquals(0,feet.toInch());
+    public void Feet_is_zero_should_return_zero_feet() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_feet(0);
+        assertEquals(0, (int) result );
     }
+
+    @org.junit.Test
+    public void null_check_for_feet() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_feet(null);
+        assertEquals("Null Value", (String) result);
+    }
+
+    @org.junit.Test
+    public void type_check_for_feet() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_feet(1);
+        boolean b = Integer.class.isInstance(result);
+        assertEquals(true, b );
+    }
+
+    @org.junit.Test
+    public void value_check_for_feet() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_feet(1);
+        assertEquals(1,(int) result);
+    }
+
+
+
+    //....................................TEST CASES FOR INCH................................................................
+    @org.junit.Test
+    public void inch_is_zero_should_return_zero_inch() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_inch(0);
+        assertEquals(0, (int) result );
+    }
+
+    @org.junit.Test
+    public void null_check_for_inch() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_inch(null);
+        assertEquals("Null Value", (String) result);
+    }
+
+    @org.junit.Test
+    public void type_check_for_inch() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_inch(1);
+        boolean b = Integer.class.isInstance(result);
+        assertEquals(true, b );
+    }
+
+    @org.junit.Test
+    public void value_check_for_inch() throws Exception
+    {
+        Object result = object_of_QM.function_for_returning_value_of_inch(1);
+        assertEquals(1,(int) result);
+    }
+
+
+
+    //.............................TEST CASE: 0 Feet and 0 Inch should return Equal....................................
+    @org.junit.Test
+    public void Equality_check_for_zero_feet_and_zero_inch() throws Exception
+    {
+        Object result1 = object_of_QM.function_for_returning_value_of_feet(1);
+        Object result2 = object_of_QM.function_for_returning_value_of_inch(1);
+        assertEquals((int) result2,(int) result1);
+    }
+
+
+
+    //..........................TEST CASES for COMPARING the LENGTHS...................................................
+    @org.junit.Test
+    public void one_feet_not_equals_one_inch() throws Exception
+    {
+        Object result1 = object_of_QM.function_for_returning_value_of_feet(1);
+        Object result2 = object_of_QM.function_for_converting_feet_toInch(result1);
+        boolean a=(result1).equals(result2);
+        assertEquals(false,a);
+    }
+
+    @org.junit.Test
+    public void one_inch_not_equals_one_feet() throws Exception
+    {
+        Object result1 = object_of_QM.function_for_converting_feet_toInch(1);
+        Object result2 = object_of_QM.function_for_returning_value_of_feet(1);
+        boolean a=(result1).equals(result2);
+        assertEquals(false,a);
+    }
+
+    @org.junit.Test
+    public void one_feet_equals_twelve_inch() throws Exception
+    {
+        Object result = object_of_QM.function_for_converting_feet_toInch(1);
+        assertEquals(12,(int) result);
+    }
+
+    @org.junit.Test
+    public void twelve_inch_equals_one_feet() throws Exception
+    {
+        Object result = object_of_QM.function_for_converting_inch_toFeet(12);
+        assertEquals(1,(int) result);
+    }
+
+
 }
